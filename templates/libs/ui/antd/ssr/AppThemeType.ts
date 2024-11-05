b055d4e18d62e9493068b7335a0a7a3d10a3bf2d
hela.ben-khalfallah <helabenkhalfallah@hotmail.fr>
2024-11-05 11:47:01 +0100

    [FEATURE]: Storybook / Utils / UI Configurations

--- templates/libs/ui/antd/ssr/AppThemeType.ts
+++ templates/libs/ui/antd/ssr/AppThemeType.ts
@@ -1,7 +1,5 @@
-import { ThemeConfig } from 'antd/es/config-provider/context';
 import { ReactNode } from 'react';
 
 export interface AppThemeProviderProps {
-    theme: ThemeConfig;
     children: ReactNode;
 }
